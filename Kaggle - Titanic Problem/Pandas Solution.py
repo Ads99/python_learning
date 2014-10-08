85a082a15372a3531f734d2b3fc664be62bbcb98
Ads99 <adam.baker1@gmail.com>
2014-10-08 17:31:54 +0100

    Finished

--- Kaggle - Titanic Problem/Pandas Solution.py
+++ Kaggle - Titanic Problem/Pandas Solution.py
@@ -171,6 +171,9 @@ df['Gender'] = df['Sex'].map({'female': 0, 'male': 1}).astype(int)
 # We do something similar for the Embarked columns
 df['Embarked_Coded'] = df['Embarked'].dropna().map({'C': 0, 'Q': 1, 'S': 2}).astype(int)
 
+# As an aside here is how to get unique values
+df['Embarked'].unique()
+
 '''
 Now it's time to deal with the missing values of Age, because most machine learning will need a
 complete set of values in that column to use it. By filling it in with guesses, we'll be introducing
@@ -186,3 +189,129 @@ For now let's decide to be more sophisticated, that we want to use the age that 
 passenger class. And decide that the median might be better. Let's build another reference table
 to calculate what each of these medians are
 '''
+
+median_ages = np.zeros((2,3))
+median_ages
+
+# We then populate the array:
+for i in range(0, 2):
+    for j in range(0, 3):
+        median_ages[i, j] = df[(df['Gender'] == i) & \
+                               (df['Pclass'] == j+1)]['Age'].dropna().median()
+
+median_ages
+
+'''
+We could fill in the missing ages directly into the Age column. But to be extra cautious and not
+lost the state of the original data, a more formal way would be to create a new column, 'AgeFill',
+and even record which ones were originally null (and thus artificially guessed)
+
+First make a copy of Age
+'''
+df['AgeFill'] = df['Age']
+
+# Take a look at just the rows with missing values and limit it to the columns important to us now
+df[ df['Age'].isnull() ][['Gender', 'Pclass', 'Age', 'AgeFill']].head(10)
+
+'''
+Use some code to fill in AgeFill based on our median_ages table. Here we happen to use the
+alternative syntax for referring to an existing column, like df.Age rather than df['Age'].
+There's a 'where' clause on df and referencing its column AgeFill, then assigning it an
+appropriate value out of median ages
+'''
+for i in range(0, 2):
+    for j in range(0, 3):
+        df.loc[ (df['Age'].isnull()) & (df['Gender'] == i) & (df['Pclass'] == j+1), 'AgeFill'] = median_ages[i, j]
+
+# Now view the same 10 rows we viewed earlier
+df[ df['Age'].isnull() ][['Gender', 'Pclass', 'Age', 'AgeFill']].head(10)
+
+'''
+This confirms we accomplished exactly what we wanted.
+
+Let's also create a feature that records whether the Age was originally missing. This is relatively simple
+by allowing pandas to use the integer conversion of the True/False evaluation of it's built-in function,
+pandas.isnull()
+'''
+
+#df['AgeIsNull'] = df['Age'].isnull() # THIS IS THE WAY I DID IT - I THINK IT'S MORE SIMPLE???
+
+df['AgeIsNull'] = pd.isnull(df['Age']).astype(int) # THIS WAY MAKES IT NUMERICAL THOUGH
+df.describe()
+
+'''
+FEATURE ENGINEERING
+
+Let's create a couple of other features, this time using simple math on existing columns. Since we know
+that "Parch" is the number of parents of children onboard, and "SibSp" is the number of siblings or
+spouses, we could collect those together as a FamilySize
+'''
+df['FamilySize'] = df['SibSp'] + df['Parch']
+
+'''
+We can also create artificial features if we think it may be advantageous to a machine learning
+algorithm -- of course, it might not. For example, we know Pclass had a large effect on survival, and
+it's possible Age will too. One artificial feature could incorporate whatever predictive power might be
+available from both Age and Pclass by multiplying them. This amplifies 3rd class (3 is a higher multiplier)
+at the same time it amplifies older ages. Both of these were less likely to survive, so in theory this
+could be useful
+'''
+df['Age*Class'] = df['Age'] * df['Pclass']
+
+# We can make some histograms of these new columns to understand them better
+df['FamilySize'].hist()
+P.show()
+
+df['Age*Class'].hist()
+P.show()
+
+'''
+We know we'd like to have better predictive power for the men, so you might be wishing you could pull
+out more information from the Name column, e.g. honorary or pedestrian title of the men. We won't
+accomplish that in this tutorial, but it might be helpful...
+'''
+
+'''
+FINAL PREPARATION
+
+We have our data almost ready for machine learning, but most basic ML techniques will not work on strings,
+and in python they almost always require the data to be an array -- the implementations we will see in the
+ "sklearn" package are not written to use a pandas dataframe. So the last two things we need to do are:
+
+ 1) determone what columns we have left which are not numeric
+ 2) send our pandas.DataFrame back to a numpy.array
+'''
+df.dtypes
+
+'''
+With a little manipulation we can require .dtypes to show only the columns which are 'object', which for
+pandas means it has strings
+'''
+df.dtypes[df.dtypes.map(lambda x: x=='object')]
+
+# The next step is to drop the columns which we will not use
+df = df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1)
+
+'''
+We can also drop 'Age' even though it's numeric since we copied and filled that to a better column: AgeFill
+The original 'Age' still has the missing values which won't work well in our future model
+'''
+df = df.drop(['Age'], axis=1)
+
+'''
+An alternate command is to drop any rows which still have missing values.
+N.B this removes observations even if only 1 column has a NaN anywhere
+It could delete most of your dataset if you aren't careful with the state of missing values in other columns
+'''
+df = df.dropna()
+
+'''
+The final step is to convert it into a Numpy array. Pandas can always send back an array
+using the .values method. Assign to a new variable, train_data
+'''
+train_data = df.values
+type(train_data)
+train_data
+
+# Compare this to the original array we used in the last tutorial!
+data
