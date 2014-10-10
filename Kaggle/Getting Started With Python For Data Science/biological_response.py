335ba0913649f7176155c4f1376c14f8f51ace06
ABaker <adam.baker1@gmail.com>
2014-10-10 17:16:05 +0100

    ?

--- Kaggle/Getting Started With Python For Data Science/biological_response.py
+++ Kaggle/Getting Started With Python For Data Science/biological_response.py
@@ -3,5 +3,9 @@ __author__ = 'ABaker'
 from sklearn.ensemble import RandomForestClassifier
 from numpy import genfromtxt, savetxt
 
-def main():
-    #create the training and test sets
+
+#def main():
+
+    # create the training and test sets
+    dataset = genfromtxt(open('Kaggle/Getting Started With Python For Data Science/train.csv', 'r'), delimiter=',', dtype=2.8f)[1:]
+    print type(dataset)
